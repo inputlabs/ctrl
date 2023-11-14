e3bf6ab44089b72a74d64a6b85efcb86486f1df7
Marcos Diaz <diazmencia@gmail.com>
2023-11-14 13:21:45 +0200

    Reset profiles on controller disconnect

--- src/services/profiles.ts
+++ src/services/profiles.ts
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-only
 // Copyright (C) 2023, Input Labs Oy.
 
+/// <reference types="w3c-web-usb" />
+
 import { Injectable } from '@angular/core'
 import { WebusbService } from 'services/webusb'
 import {
@@ -34,6 +36,10 @@ export class ProfileService {
     private webusb: WebusbService,
   ) {
     this.initProfiles()
+    // Reset profiles if controller is disconnected.
+    navigator.usb.addEventListener('disconnect', (event:any) => {
+      this.initProfiles()
+    })
   }
 
   initProfiles() {
