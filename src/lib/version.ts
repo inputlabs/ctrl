9f0b0c77be79bd688df9ab3fcbd24dc2e6293383
Marcos Diaz <diazmencia@gmail.com>
2023-12-27 12:13:09 +0200

    Uppercase names in version constants

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,6 +1,6 @@
 // App version placeholders.
-export const version = '0.0.0'
-export const commit = 'abcdef123456'
+export const VERSION = '0.0.0'
+export const COMMIT = 'abcdef123456'
 
-// Controller firmware version
+// Controller firmware version.
 export const LATEST_FIRMWARE = [0, 94, 0]
