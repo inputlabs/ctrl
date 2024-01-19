6bfe06ece39fca99d0b6c5d211ef7691f8029071
Marcos Diaz <diazmencia@gmail.com>
2024-01-19 17:25:50 +0200

    Bump firmware version notification

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -3,4 +3,4 @@ export const VERSION = '0.0.0'
 export const COMMIT = 'abcdef123456'
 
 // Controller firmware version.
-export const LATEST_FIRMWARE = [0, 94, 0]
+export const LATEST_FIRMWARE = [0, 95, 0]
